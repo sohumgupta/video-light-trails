75528c714614000ed95fb506e105a839f2efc8c8
zachooz <hoffking@gmail.com>
2020-11-05 16:32:57 -0500

    ability  to save live video

--- main.py
+++ main.py
@@ -8,10 +8,15 @@ class VideoGet:
 	with a dedicated thread.
 	"""
 
-	def __init__(self, src=0):
+	def __init__(self, src=0, save_video=False):
 		self.stream = cv2.VideoCapture(src)
 		(self.grabbed, self.frame) = self.stream.read()
 		self.stopped = False
+		if save_video:
+			frame_width = int(self.stream.get(3)) 
+			frame_height = int(self.stream.get(4)) 
+			self.size = (frame_width, frame_height) 
+			self.result = cv2.VideoWriter('filename.avi', cv2.VideoWriter_fourcc(*'MJPG'), 10, self.size) 
 
 	def start(self):
 		Thread(target=self.get, args=()).start()
@@ -28,7 +33,8 @@ class VideoGet:
 		self.stopped = True
 
 if __name__ == "__main__":
-	video_getter = VideoGet(0).start()
+	save_video = False
+	video_getter = VideoGet(0, save_video).start()
 
 	echoes = []
 	num_echoes = 8
@@ -44,12 +50,14 @@ if __name__ == "__main__":
 
 		# Our operations on the frame come here
 		echoes_np = np.array(echoes)
-		out = (np.max(echoes_np, axis=0))
-		# out = np.mean(echoes_np, axis=0)).astype(np.uint8)
-		# out = ((np.max(echoes_np, axis=0) * 0.8 + np.mean(echoes_np, axis=0) * 0.2)).astype(np.uint8)
+		# out = (np.max(echoes_np, axis=0))
+		# out = np.mean(echoes_np, axis=0).astype(np.uint8)
+		out = ((np.max(echoes_np, axis=0) * 0.8 + np.mean(echoes_np, axis=0) * 0.2)).astype(np.uint8)
 
 		# Display the resulting frame
 		cv2.imshow('frame', out)
+		if save_video:
+			video_getter.result.write(out)
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			video_getter.stop()
 			break
